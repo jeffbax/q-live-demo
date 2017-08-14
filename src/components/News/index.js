7ba7705f56af15061fc232359d665463ee1770fa
Jeff Baxendale <jeffbax@gmail.com>
2017-08-14 11:46:59 -0400

    news from reddit!

--- src/components/News/index.js
+++ src/components/News/index.js
@@ -1,9 +1,31 @@
 import React, { Component } from 'react';
 
+class NewsItem extends Component {
+  get article() {
+    return this.props.article;
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>
+          <a href={`https://www.reddit.com/${this.article.permalink}`}>
+            {this.article.title}
+          </a>
+        </h3>
+        <p>
+          {this.article.thumbnail !== "self" ? <img src={this.article.thumbnail} alt="thumbnail" /> : ""}
+          Posted by: <a href={`https://www.reddit.com/user/${this.article.author}`}>{this.article.author}</a>
+        </p>
+      </div>
+    )
+  }
+}
+
 class News extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {articles: []};
   }
 
   async fetchLatest() {
@@ -11,12 +33,30 @@ class News extends Component {
     console.log(results);
   }
 
+  componentDidMount() {
+    this.loadLatestNews();
+  }
+
+  loadLatestNews() {
+    // Loads json script (to get around CORS)
+    const timeStamp = `ql${Date.now()}`;
+    const script = document.createElement('script');
+    script.src = `https://www.reddit.com/r/quake/.json?jsonp=${timeStamp}`;
+
+    window[timeStamp] = (jsonData) => {
+      this.setState({articles: jsonData.data.children});
+      delete window[timeStamp];
+      document.head.removeChild(script);
+    }
+
+    document.head.appendChild(script);
+  }
+
   render() {
-    return (
-      <div>
-        <h2>Quake News:</h2>
-        <a onClick={this.fetchLatest.bind(this)}>Reload</a>
-      </div>
+    return this.state.articles.length < 1 ? <p>Loading &hellip;</p> : (
+      <ul>
+        {this.state.articles.map(a => <li key={a.data.id}><NewsItem article={a.data} /></li>)}
+      </ul>
     );
   }
 }
--- src/routes/Home/index.js
+++ src/routes/Home/index.js
@@ -7,6 +7,10 @@ class Home extends Component {
   render() {
     return (
       <div className="QLive">
+        <h2>
+          The Latest Quake News
+          <img src="/Reddit-PNG-HD.png" alt="Thanks Reddit!" />
+        </h2>
         <News />
       </div>
     );
